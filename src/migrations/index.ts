d829c3d84e3bf6e9e3ad0a3dcf12736a73ab5e2f
arbitroy <ndauwa.austine20@students.dkut.ac.ke>
2026-05-08 13:07:46 +0300

    run migrations

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20260508_073200 from './20260508_073200';
+import * as migration_20260508_100635 from './20260508_100635';
 
 export const migrations = [
   {
     up: migration_20260508_073200.up,
     down: migration_20260508_073200.down,
-    name: '20260508_073200'
+    name: '20260508_073200',
+  },
+  {
+    up: migration_20260508_100635.up,
+    down: migration_20260508_100635.down,
+    name: '20260508_100635'
   },
 ];
