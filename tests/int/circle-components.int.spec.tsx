af916ec2456c821477f5d899f917c6863935bc9e
arbitroy <ndauwa.austine20@students.dkut.ac.ke>
2026-05-28 11:27:51 +0300

    chore: remove unused normalizeColor helper from circle-components tests

--- tests/int/circle-components.int.spec.tsx
+++ tests/int/circle-components.int.spec.tsx
@@ -3,16 +3,6 @@ import { render } from '@testing-library/react'
 import React from 'react'
 import { SectionScoop } from '@/components/ui/SectionScoop'
 
-// Helper to normalize color values for comparison
-// jsdom converts HSL and named colors to RGB
-function normalizeColor(color: string): string {
-  // If it's already in rgb format, return as-is
-  if (color.startsWith('rgb')) return color
-  // If it's an HSL color, jsdom will have converted it to rgb
-  // For testing, we just ensure the color value was set (non-empty)
-  return color
-}
-
 // jsdom does not ship IntersectionObserver
 beforeEach(() => {
   global.IntersectionObserver = vi.fn().mockImplementation(() => ({
