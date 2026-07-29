d0054033132707ed0baae0ab3053f2632347ced5
arbitroy <ndauwa.austine20@students.dkut.ac.ke>
2026-07-29 10:14:31 +0300

    Add unoptimized images configuration in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,7 @@ const dirname = path.dirname(__filename)
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true,  // ADD THIS LINE
     formats: ['image/avif', 'image/webp'],
     minimumCacheTTL: 60 * 60 * 24 * 30, // 30 days
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
